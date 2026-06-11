f26feb4ecde35541806c600663c8bf4c153d9b90
KimChangmin <zzangmin3@gmail.com>
2026-06-11 10:20:18 +0900

    next 설정에 주석 추가

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     remotePatterns: [
+      // 이미지를 허용하는 URL 설정(1. Github 이미지 허용, 2. 이미지 서버 생성 후 수정 예정)
       {
         protocol: "https",
         hostname: "raw.githubusercontent.com",
